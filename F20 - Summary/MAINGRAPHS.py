f7603aac7107aff4bb6dba5c9c917961ae6d2c92
zachang890 <zachang890@gmail.com>
2020-12-10 20:56:43 -0800

    Add code for all graphs, time to produce graphs

--- F20 - Summary/MAINGRAPHS.py
+++ F20 - Summary/MAINGRAPHS.py
@@ -2,6 +2,7 @@
 
 from graph_train_test import Graph_Train_Test
 from atomic_mol_percentage import Atomic_Mol_Percentage
+from alpha_finder import Alpha_Finder
 
 if __name__ == "__main__":
 
@@ -11,3 +12,32 @@ if __name__ == "__main__":
     grapher = Graph_Train_Test(atomic_obj.retrieve_compound_names())
     y_actual_test, y_pred_test, y_actual_train, y_pred_train = grapher.optimal_regress(15)
     grapher.plot_expectedvactual(y_actual_test, y_pred_test, y_actual_train, y_pred_train, "Predicted Poisson's Ratio vs. Actual Poisson's Ratio - GRAPH 1 (chemical comp.)")
+
+    # 2. #################### GRAPH 2: Regression Using First-Level Transform & No Feature Selection
+
+    alpha_obj = Alpha_Finder("./resources/transformed_samples_by_features.csv")
+    y_actual_test, y_pred_test, y_actual_train, y_pred_train = grapher.optimal_regress_selective(15, alpha_obj.all_feature_names)
+    grapher.plot_expectedvactual(y_actual_test, y_pred_test, y_actual_train, y_pred_train,
+                                 "Predicted Poisson's Ratio vs. Actual Poisson's Ratio - GRAPH 2 (w/ transform & no selection)")
+
+    # 3. #################### GRAPH 3: Regression Using First-Level Transform & Feature Selection
+
+    #EXCERPT FROM MAIN2.py
+    poisson_train, poisson_test = alpha_obj.calculate_alpha_rmse("POISSONS")
+    poisson_average_rmse_train, poisson_average_rmse_test = alpha_obj.average_rmse(poisson_train, poisson_test)
+    alpha_obj.plot_rmse_alpha(poisson_average_rmse_train, poisson_average_rmse_test, "Poisson's Ratio", "g")
+    poisson_coeffs = alpha_obj.lasso_coeffs(10 ** (-6), "Poisson's ratio v", "Poisson's Ratio")
+
+    poisson_coeffs_dict = alpha_obj.coeffs_to_dict(poisson_coeffs)
+    poisson_coeffs_dict = alpha_obj.filter_coeff_dicts(poisson_coeffs_dict)
+    poisson_ranked = alpha_obj.rank_coeffs(poisson_coeffs_dict)
+    poisson_selected = alpha_obj.select_features(poisson_ranked)
+
+    poisson_selected = poisson_selected[:50]
+    y_actual_test, y_pred_test, y_actual_train, y_pred_train = grapher.optimal_regress_selective(15, poisson_selected)
+    grapher.plot_expectedvactual(y_actual_test, y_pred_test, y_actual_train, y_pred_train,
+                                 "Predicted Poisson's Ratio vs. Actual Poisson's Ratio - GRAPH 3 (w/ transform & w/ selection)")
+
+
+
+
--- F20 - Summary/graph_train_test.py
+++ F20 - Summary/graph_train_test.py
@@ -8,7 +8,7 @@ class Graph_Train_Test:
     def __init__(self, names):
         self.names = names
 
-    def optimal_regress(self, estimators):
+    def optimal_regress(self, estimators): #Predicting Poisson's Ratio
         pred = pd.read_csv("./resources/Oxide_glass_1_5_02142020.csv").drop(
             ["Index", "Code", "Glass #", "Author", "Year", "Trademark", "Glass_composition", "Young's modulus E (GPa)",
              "Shear modulus G (GPa)"], axis=1)
@@ -45,3 +45,27 @@ class Graph_Train_Test:
         plt.plot(x, y)
         plt.legend()
         plt.show()
+
+    def optimal_regress_selective(self, estimators, selected): # GRAPH 2 and 3
+        selected_data = pd.read_csv("./resources/transformed_samples_by_features.csv").drop(columns=["Unnamed: 0"])
+        selected_features_df = pd.DataFrame()
+        for feature in selected:
+            selected_features_df[feature] = selected_data[feature]
+        selected_features_df
+
+        pred = pd.read_csv("./resources/Oxide_glass_1_5_02142020.csv").drop(
+            ["Index", "Code", "Glass #", "Author", "Year", "Trademark", "Glass_composition", "Young's modulus E (GPa)",
+             "Shear modulus G (GPa)"], axis=1)
+        X = selected_features_df.iloc[:, 0:len(selected)]
+        Y = pred["Poisson's ratio v"]
+
+        X_train, X_test, y_actual_train, y_actual_test = train_test_split(X, Y, test_size=0.2, random_state=30)
+        regressor = RandomForestRegressor(n_estimators=estimators, random_state=0)
+        regressor.fit(X_train, y_actual_train)
+        y_pred_test = regressor.predict(X_test)
+
+        regressor = RandomForestRegressor(n_estimators=estimators, random_state=0)
+        regressor.fit(X_train, y_actual_train)
+        y_pred_train = regressor.predict(X_train)
+
+        return y_actual_test, y_pred_test, y_actual_train, y_pred_train
